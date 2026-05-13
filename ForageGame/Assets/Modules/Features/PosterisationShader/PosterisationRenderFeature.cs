029d83732b85982a27aa27896a1bb091ca476cab
RobinPino <r.a.pino@student.tue.nl>
2026-05-13 14:37:04 +0200

    Fix player swim fast wonkiness

---
+++ ForageGame/Assets/Modules/Features/PosterisationShader/PosterisationRenderFeature.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PosterisationRenderFeature : ScriptableRendererFeature
+{
+    PosterisationPass _pass;
+    
+    public RenderPassEvent injectionPoints;
+
+    public List<Color> palette;
+    
+    public override void Create()
+    {
+        _pass = new PosterisationPass(palette);
+        _pass.renderPassEvent = injectionPoints;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(_pass);
+    }
+}
