0395254b916ab2ac62c4c28144893cb749189807
Timmerdebim <timmerdebim@gmail.com>
2026-06-06 15:18:22 +0200

    gadget fix

--- ForageGame/Assets/Modules/Features/Chase Item/ChasingItem.cs
+++ ForageGame/Assets/Modules/Features/Chase Item/ChasingItem.cs
@@ -7,25 +7,19 @@ public class ChasingItem : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float playerEffectRadius = 4;
     [SerializeField] private float playerForceStrength = 10;
-    [SerializeField] private float coreForceStrength = 5;
-    private Vector3 corePos;
     private float playerForceOffset;
 
     void Awake()
     {
-        corePos = transform.position;
         playerForceOffset = playerForceStrength * playerEffectRadius;
     }
 
+    private Vector3 r;
+
     void FixedUpdate()
     {
-        Vector3 playerForce = (transform.position - Player.Instance.transform.position).normalized;
-        playerForce.y = 0;
-        playerForce *= (-playerForceStrength * Vector3.Distance(transform.position, Player.Instance.transform.position) + playerForceOffset);
-        Vector3 coreForce = (corePos - transform.position).normalized;
-        coreForce.y = 0;
-        coreForce *= coreForceStrength * Vector3.Distance(transform.position, corePos);
-        rb.AddForce(playerForce, ForceMode.Acceleration);
-        rb.AddForce(coreForce, ForceMode.Acceleration);
+        r = transform.position - Player.Instance.transform.position;
+        if (r.magnitude < playerEffectRadius)
+            rb.AddForce(r.normalized * (-playerForceStrength * Vector3.Distance(transform.position, Player.Instance.transform.position) + playerForceOffset), ForceMode.Acceleration);
     }
 }
