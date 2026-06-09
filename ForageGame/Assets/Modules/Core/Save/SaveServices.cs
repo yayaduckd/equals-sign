e19159de5a73e20e324196c340f949e6596c1fc4
Timmerdebim <timmerdebim@gmail.com>
2026-06-09 13:00:21 +0200

    fix???
    why is screen black?????

--- ForageGame/Assets/Modules/Core/Save/SaveServices.cs
+++ ForageGame/Assets/Modules/Core/Save/SaveServices.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using System;
-using UnityEditor.Overlays;
+// using UnityEditor.Overlays;
 
 namespace TDK.SaveSystem
 {
