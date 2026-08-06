0b23e4afc95f9dae0519a76a2e7fe259aae9c898
HaentjeGH <l.m.j.haenen@student.tue.nl>
2026-08-06 17:57:02 +0200

    Unity physics strike again

--- ForageGame/Assets/Shaders/Darkness/LanternController.cs
+++ ForageGame/Assets/Shaders/Darkness/LanternController.cs
@@ -8,6 +8,7 @@ using Weather;
 public class PlayerLanternController : MonoBehaviour
 {
     [SerializeField] private Light _light;
+    [SerializeField] private Renderer glassRenderer;
 
     [SerializeField] private Color mutedColor = Color.red;
     [SerializeField] private Color BrightColor = Color.yellow;
@@ -15,11 +16,16 @@ public class PlayerLanternController : MonoBehaviour
     [SerializeField] private float minIntensity = 0f;
     [SerializeField] private float maxIntensity = 5f;
 
-    [SerializeField][Range(0f, 1f)] private float lanternStrength = 0.5f;
+    [SerializeField][Range(0f, 1f)] private float lanternStrength01 = 0.5f;
 
     [SerializeField][Range(0f, 10f)] private float flickerSpeed = 1f;
+    [SerializeField][Range(0f, .4f)] private float flickerScale = .2f;
 
     [SerializeField] FBM1D fbm = new FBM1D(FBM1D.NoiseFunctionType.Sin, 4, 1.97f, 0.43f);
+
+    private MaterialPropertyBlock propertyBlock = new MaterialPropertyBlock();
+
+    private static readonly int FlickerID = Shader.PropertyToID("_Flicker");
 
 
     private float weight = 0f;
@@ -29,17 +35,24 @@ public class PlayerLanternController : MonoBehaviour
         weight = WeatherManager.Instance.lanternIntensity; //weathermanager decides relative lantern strength
         if(weight > 0f)
         {
-            lanternStrength = fbm.Eval01(Time.time * flickerSpeed);
+            lanternStrength01 = fbm.Eval01(Time.time * flickerSpeed);
             SetLanternVisuals();
         }
-        else _light.intensity = 0f;
+        else 
+        {
+            _light.intensity = 0f;
+        }
     }
 
     [ContextMenu("Set Lantern Visuals")]
     private void SetLanternVisuals()
     {
-        _light.color = Color.Lerp(mutedColor, BrightColor, lanternStrength);
-        _light.intensity = Mathf.Lerp(minIntensity*weight, maxIntensity*weight, lanternStrength);
+        _light.color = Color.Lerp(mutedColor, BrightColor, lanternStrength01);
+        _light.intensity = Mathf.Lerp(minIntensity*weight, maxIntensity*weight, lanternStrength01);
+
+        glassRenderer.GetPropertyBlock(propertyBlock);
+        propertyBlock.SetFloat(FlickerID, (lanternStrength01 + 1f)*flickerScale);
+        glassRenderer.SetPropertyBlock(propertyBlock);
     }
 
 }
